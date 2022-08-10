87dc67c400d0b4f7aa403a8118bda5397ad1e25a
Javier Vargas <3231javiervargas@gmail.com>
2022-08-10 13:02:17 -0400

    debugging

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const sum = (a,b) => {
-    return a+b
+export const sum = (a, b) => {
+  if (!a || !b) {
+    return "The input is incorrect!"
+  } else if (typeof a !== "number" || typeof b !== "number") {
+    return "Only number are allowed"
+  }
+
+  return a + b
+}
+
+export const mult = (a, b)=>{
+    if (!a || !b) {
+        return "The input is incorrect!"
+      } else if (typeof a !== "number" || typeof b !== "number") {
+        return "Only number are allowed"
+      }
+    
+      return a + b
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,43 @@
-import {sum} from "../src/index.js"
+import { sum, mult } from "../src/index.js"
 
-it("should add 2 numbers and return the sun", ()=>{
+it("should add 2 numbers and return the sun", () => {
+  //Arrage
+  const a = 4,
+    b = 5,
+    expected = 9
+
+  //Act
+  const actual = sum(a, b)
+
+  //Assert
+  expect(actual).toBe(expected)
+})
+
+it("should add a number and null and return a mystery", () => {
+  //Arrage
+  const a = null,
+    b = 5,
+    expected = "The input is incorrect!"
+
+  //Act
+  const actual = sum(a, b)
+
+  //Assert
+  expect(actual).toBe(expected)
+})
+
+it("should add a number and string and return a error", () => {
     //Arrage
-    const a = 4, b = 5, expected = 9
-
+    const a = "str",
+      b = 5,
+      expected = 'are allowed'
+  
     //Act
     const actual = sum(a, b)
-
+  
     //Assert
-    expect(actual).toBe(expected)
-})
+    expect(actual).toContain(expected)
+  })
+  
+
+  //set it up for multipy
